815d3e7261e008275e5f80af5405d9929a6b17a8
SimonOkello <simonokello93@gmail.com>
2018-12-11 22:59:27 +0300

    modified

--- calculator.py
+++ calculator.py
@@ -1,16 +1,4 @@
-import unittest
-class Calculate(object):
- def add(self, x, y):
-     return x + y
-
-class TestCalc(unittest.TestCase):
-    def testAdd(self):
-        calc = Calculate()
-        result = calc.add(2, 2)
-        self.assertEqual(result, 4)
-        print(result)
-
-if __name__ == '__main__':
-    unittest.main()
-
-
+class Calculator(object):
+    def add(self, x, y):
+        return x + y
+        
--- test_calculator.py
+++ test_calculator.py
@@ -3,10 +3,10 @@ from calculator import Calculator
 class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
+        
     def testAdd(self):
+        result = self.calc.add(4, 7)
+        self.assertEqual(result, 11)
 
-              result = self.calc.add(4, 7)
-              self.assertEqual(result, 11)
-
-    if __name__ == "main":
-        unittest.main()
+if __name__ == "main":
+    unittest.main()
