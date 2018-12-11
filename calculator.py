e258595fcfaeefcfada03d5de6dac6771ef591ef
SimonOkello <simonokello93@gmail.com>
2018-12-11 15:31:07 +0300

    modified calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,16 @@
-class Arithmetic(object):
-    def Calculator(self, x ,y):
-        return x + y
+import unittest
+class Calculate(object):
+ def add(self, x, y):
+     return x + y
+
+class TestCalc(unittest.TestCase):
+    def testAdd(self):
+        calc = Calculate()
+        result = calc.add(2, 2)
+        self.assertEqual(result, 4)
+        print(result)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
